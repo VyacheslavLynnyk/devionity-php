4694a6ba5e655f48a76924e3e99c62171ad91965
Vyacheslav Lynnyk <vyacheslav.lynnyk@gmail.com>
2016-02-06 23:04:04 +0200

    Homework on devionity 1 - 20

---
+++ module-1/19-arrays.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Arrays</title>
+</head>
+<body>
+<h1>Arrays</h1>
+<pre>
+    <?php
+    $array = [
+        "Австралия"	=>"Канберра",
+        "Австрия" =>"Вена",
+        "Азербайджан" => "Баку",
+        "Албания"	=> "Тирана",
+        "Алжир"	=>" Алжир",
+        "Ангола" => "Луанда",
+    ];
+        print_r($array);
+    ?>
+</pre>
+</body>
+</html>
---
+++ module-1/php-test.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Php Start</title>
+</head>
+<body>
+    <?php
+        phpinfo();
+
+    ?>
+    <p><?= 45?></p>
+</body>
+</html>
