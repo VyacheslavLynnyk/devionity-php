2132596bc5193d27b96d0fec8245bda397aa4464
Vyacheslav Lynnyk <vyacheslav.lynnyk@gmail.com>
2016-03-02 01:51:50 +0200

    Notepad (with tree) v1.1 beta

--- module-1/notepad/func.lib.php
+++ module-1/notepad/func.lib.php
@@ -1,18 +1,61 @@
 <?php
 
-//$index = explode('/',$_SERVER['PHP_SELF']);
-//array_pop($index);
 define('BASE_URL', '.');
-//    . implode('/', $index));
+define ('CONTAINER', __DIR__.'/container');
 
-function editFile($path)
+
+function getPathArr($path)
+{   if (strpos($path, '/..')) {
+        return [];
+    }
+    $pathArr = explode('/', $path);
+    if (is_file(CONTAINER . $path) || !is_dir((CONTAINER . $path))) {
+        array_pop($pathArr);
+    }
+    return ($pathArr) ? $pathArr : [];
+}
+
+function getPartPath($pathArr, $level )
+{
+    if (sizeof($pathArr) <= $level ) {
+        return false;
+    }
+    $path = '';
+    $level++;
+    for ($num = 0; $num < $level ; $num++ ) {
+        $path = $path . $pathArr[$num] . '/';
+    }
+    return $path;
+}
+
+function getFilesArr($pathArr) {
+    $scanPath = '';
+
+    if (!isset($pathArr[0])) {
+        $pathArr[0] = '';
+    }
+    foreach ($pathArr as $num => $value) {
+
+        $scanPath = $scanPath . $value . '/' ;
+        $filesArr[$num] = scandir(CONTAINER . $scanPath);
+        // remove '.' and '..'
+        array_shift($filesArr[$num]);
+        array_shift($filesArr[$num]);
+    }
+    return $filesArr;
+}
+
+function saveFile()
 {
     if (isset($_POST['edit'])) {
-        if (is_dir($_POST['filename'])) {
-//            rename($_POST['filename'], $path. '/' .$_POST['content']);
-            echo "dir $path";
-        } elseif (is_file($_POST['filename'])) {
-            file_put_contents($_POST['filename'], $_POST['content']);
+        if (is_dir(CONTAINER . $_POST['filename'])) {
+            $linkArr = explode('/', $_POST['filename']);
+            array_pop($linkArr);
+            $link = implode('/', $linkArr) . '/';
+            rename(CONTAINER . $_POST['filename'], CONTAINER . $link . $_POST['content']);
+            $_POST['filename'] = $link . $_POST['content'];
+        } elseif (is_file(CONTAINER . $_POST['filename'])) {
+            file_put_contents(CONTAINER . $_POST['filename'], $_POST['content']);
         }
     }
 }
@@ -28,6 +71,28 @@ function renameIt($path)
     }
 }
 
+function showFilesPath($filesArr, $pathArr, $level = 0)
+{
+    ?> <ul> <?php
+    if ($level >= (sizeof($pathArr))) {
+        ?> </ul> <?php
+        return;
+    }
+    $link = getPartPath($pathArr, $level);
+    foreach ($filesArr[$level] as $key => $file) {
+        if (is_dir(CONTAINER . $link . $file)) {
+            ?> <li class="folder"><a href="?filename=<?= $link . $file ?>"><?= $file ?></a></li> <?php
+        } else {
+            ?> <li class="file"><a href="?filename=<?= $link . $file ?>"><?= $file ?></a></li> <?php
+        }
+        if (isset($pathArr[$level+1]) && $file == $pathArr[$level+1]) {
+            ?> <li><?php showFilesPath($filesArr, $pathArr, ++$level) ?></li> <?php
+        }
+    }
+    ?> </ul> <?php
+}
+
+/*
 function scanFolder($path, $link)
 {
     $dir = scandir($path.'/');
@@ -57,13 +122,19 @@ function scanFolder($path, $link)
     }
     ?> </ul> <?php
 }
+*/
+
 
 // ----
+saveFile();
 
-    $path = __DIR__ . '/container';
-    $link = BASE_URL . '/container';
+if ( !isset($_GET['filename']) ||
+     !is_dir(CONTAINER . $_GET['filename']) && !is_file(CONTAINER . $_GET['filename']) ) {
 
-    editFile($path);
+        $_GET['filename'] = (isset($_POST['filename'])) ? $_POST['filename'] : null;
+}
+$path = $_GET['filename'];
 
 
-    ?>
+
+?>
--- module-1/notepad/index.php
+++ module-1/notepad/index.php
@@ -3,84 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Notepad</title>
-    <style>
-        body {
-            margin:0;
-            padding: 0;
-        }
-        html, body, form, .container {
-            height: 100%;
-        }
-        table {
-            margin: auto;
-            width: 90%;
-            height: 100%;
-            border-collapse: collapse;
-        }
-        .textwrapper {
-            border:1px solid #999999;
-            margin:5px 0;
-            padding:3px;
-            height: 100%;
-        }
-        #text {
-            display: block;
-            width: 100%;
-            min-height: 600px;
-            height: inherit;
-            -webkit-box-sizing: border-box;
-            -moz-box-sizing: border-box;
-            box-sizing: border-box;
-        }
-
-        #folders {
-            background: lightsteelblue;
-            /*padding: 3px;*/
-            width: 20%;
-            vertical-align: top;
-        }
-        #note-head {
-            background: lightsteelblue;
-
-        }
-        .folder {
-            border-top: 1px dotted brown;
-            color: brown;
-        }
-        .folder, .file {
-            display: block;
-            padding: 13px 5px 2px 10px;
-            /*width: 100%;*/
-            /*height: 20px;*/
-            background: white;
-        }
-        .file {
-            color: #3056a0;
-        }
-        .rename {
-            visibility: hidden;
-            text-align: right;
-            padding-left: 30px;
-            font-size: 10px;
-        }
-        #folders > .folder:hover > .rename:first-child,
-        .folder > .file:hover > .rename:first-child,
-        .folder > .folder:hover > .rename:first-child {
-            visibility: visible;
-            link: green;
-        }
-        .logo {
-            padding: 10px;
-            font-size: 2em;
-            text-align: right;
-        }
-
-    </style>
+    <link rel="stylesheet" href="style-notepad.css">
 </head>
 <body>
 <?php include_once __DIR__ . '/func.lib.php'; ?>
     <div class="container">
-        <form action="<?= $_SERVER['PHP_SELF']?>" method="POST">
+        <form action="<?= $_SERVER['PHP_SELF'].'?filename='.$path ?>" method="POST">
         <table>
             <tr id="note-head">
                 <td><input type="submit" name="edit" value="Save"></td>
@@ -88,25 +16,21 @@
             </tr>
             <tr>
                 <td id="folders">
-                    <?php scanFolder($path, $link);
-//                    if (isset($_GET['filename'])) {
-//                        $filename = $_GET['filename'];
-//                        if (is_dir($filename)) {
-//                            scanFolder($filename, $filename);
-//                        }
-//                    }
-                    //print_r($filename);?>
+                   <?php
+                   $pathArr = getPathArr($path);
+                   $filesArr = getFilesArr($pathArr);
+                   showFilesPath($filesArr, $pathArr);
+                   ?>
                 </td>
-                <td>
+                <td id="textfield">
                     <?php if (isset($_GET['filename'])) : ?>
-                        echo 'ok';
                         <div class="textwrapper">
                             <textarea name="content" id="text"><?php
-                                $filename = $_GET['filename'];
-
-                                 if (is_file($filename)) {
-                                    $file = file_get_contents(__DIR__.'/container/file3.txt');
+                                if (is_file(CONTAINER . $path)) {
+                                    $file = file_get_contents(CONTAINER . $path);
                                     echo $file;
+                                } elseif (is_dir(CONTAINER . $path)) {
+                                    echo end($pathArr);
                                 }
                           ?></textarea>
 
@@ -117,7 +41,7 @@
                 </td>
             </tr>
             <tr>
-                <td colspan="2"> status bar </td>
+                <td colspan="2"> <!-- status bar --> &nbsp;</td>
             </tr>
         </table>
         </form>
